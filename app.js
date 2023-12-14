00a7540c41cb87ce6a92682b297acaf306789fed
minthantkyaw28 <wanderhobby@gmail.com>
2023-12-14 11:42:40 +0630

    egt4g

--- app.js
+++ app.js
@@ -1164,6 +1164,116 @@ app.post("/request-stock", async (req, res) => {
   return res.json(data);
 });
 
+app.post("/add-stock", async (req, res) => {
+  const product_name = req.body?.product_name;
+  const quantity = req.body?.quantity;
+
+  if (
+    !product_name ||
+    typeof product_name !== "string" ||
+    !quantity ||
+    typeof quantity !== "number"
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Product Name and Quantity are required!" });
+  }
+
+  const foundProduct = await products.findOne({ product_name });
+  if (!!foundProduct) {
+    return res.status(400).json({ message: "Product already exists!" });
+  }
+
+  const result = await products.insertOne({
+    product_name,
+    in_stock_count: quantity,
+  });
+  if (!result) {
+    return res.status(500).json({ message: "Something went wrong!" });
+  }
+  return res.json(result);
+});
+
+app.delete("/remove-stock/:id", async (req, res) => {
+  const productId = req.params?.id;
+  if (!productId) {
+    return res.status(400).json({ message: "Product ID is required!" });
+  }
+  if (!ObjectId.isValid(productId)) {
+    return res.status(400).json({ message: "Invalid product ID!" });
+  }
+
+  const result = await products.findOneAndDelete({
+    _id: new ObjectId(productId),
+  });
+  if (!result) {
+    return res.status(400).json({ message: "Product not found!" });
+  }
+  return res.json({ message: "Product deleted successfully!" });
+});
+
+app.get("/requested-stocks/:date", async (req, res) => {
+  const inputDateString = req.params?.date;
+  if (!inputDateString) {
+    return res.status(400).json({ message: "Date is required!" });
+  }
+
+  const currentDate = new Date();
+  const inputDate = new Date(inputDateString);
+
+  const startDayDate = new Date(inputDate.toISOString());
+  startDayDate.setHours(0, 0, 0, 0);
+
+  const sameYear = inputDate.getFullYear() === currentDate.getFullYear();
+  const sameMonth = inputDate.getMonth() === currentDate.getMonth();
+  const sameDay = inputDate.getDate() === currentDate.getDate();
+
+  if (!sameYear || !sameMonth || !sameDay) {
+    inputDate.setHours(23, 59, 59, 999);
+  } else {
+    inputDate.setHours(
+      currentDate.getHours(),
+      currentDate.getMinutes(),
+      currentDate.getSeconds(),
+      currentDate.getMilliseconds()
+    );
+  }
+
+  const data = await stock_requests
+    .aggregate([
+      {
+        $match: {
+          created_date: {
+            $gte: startDayDate,
+            $lte: inputDate,
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "product_id",
+          foreignField: "_id",
+          as: "product",
+        },
+      },
+      {
+        $unwind: {
+          path: "$product",
+          includeArrayIndex: "index",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          index: 0,
+          product_id: 0,
+        },
+      },
+    ])
+    .toArray();
+  return res.json(data);
+});
 //--------------- --------------- ----- --------------- ---------------
 //--------------- --------------- Admin --------------- ---------------
 //--------------- --------------- ----- --------------- ---------------
